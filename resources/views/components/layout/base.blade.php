b099897bee4655e7b2034a6479fcf4f4625ad30e
Alex Dzyuba <alexeydzyuba27@gmail.com>
2021-07-23 22:23:12 +0300

    work

--- resources/views/components/layout/base.blade.php
+++ resources/views/components/layout/base.blade.php
@@ -3,6 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" href="/favicon.ico">
 {{--  <script defer src="/js/init.js"></script>--}}
 {{--  <script defer src="/js/app.js"></script>--}}
 
